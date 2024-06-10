df474af2c4d443ea40af4cd178729726d118a754
Nobisuke19 <subham019650@gmail.com>
2024-06-11 02:03:26 +0530

    filtering,sorting,limiting,pagination

--- server/controller/productCtrl.js
+++ server/controller/productCtrl.js
@@ -1,3 +1,4 @@
+const { json } = require('body-parser')
 const Product = require('../models/productModel')
 const asyncHandler = require('express-async-handler')
 const slugify = require('slugify')
@@ -35,8 +36,8 @@ const updateProduct = asyncHandler(async (req, res) => {
     }
 });
 
-const deleteProduct = asyncHandler(async(req,res) => {
-    const {id}  = req.params; 
+const deleteProduct = asyncHandler(async (req, res) => {
+    const { id } = req.params;
     try {
 
         const deleteProduct = await Product.findOneAndDelete(id)
@@ -60,8 +61,42 @@ const getProduct = asyncHandler(async (req, res) => {
 
 const getAllProduct = asyncHandler(async (req, res) => {
     try {
-        const getAllProducts = await Product.find();
-        res.json(getAllProducts);
+        const queryObj = { ...req.query };
+        const excludeFields = ["page", "sort", "limit", "fields"];
+        excludeFields.forEach((elm) => delete queryObj[elm]);
+        let queryStr = JSON.stringify(queryObj);
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+        let query = Product.find(JSON.parse(queryStr));
+
+        //sorting
+        if(req.query.sort){
+            const sortBy = req.query.sort.split(",").join(' ');
+            query = query.sort(sortBy)
+        }else{
+            query = query.sort('-createdAt')
+        }
+
+        //limiting fields
+        if (req.query.fields) {
+            const fields = req.query.fields.split(",").join(" ");
+            query = query.select(fields);
+          } else {
+            query = query.select("-__v");
+          }
+
+          //pagination
+          const page = req.query.page;
+          const limit = req.query.limit;
+          const skip = (page - 1) * limit;
+          query = query.skip(skip).limit(limit);
+          if (req.query.page) {
+            const productCount = await Product.countDocuments();
+            if (skip >= productCount) throw new Error("This Page does not exists");
+          }
+
+        const product = await query;
+        res.json(product);
     } catch (error) {
         throw new Error(error)
     }
