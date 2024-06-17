45d4a92354df2569bcd28f49b23f2501be17cb83
Nobisuke19 <subham019650@gmail.com>
2024-06-17 14:39:51 +0530

    update-forgot-reset password

--- server/controller/userCtrl.js
+++ server/controller/userCtrl.js
@@ -4,6 +4,8 @@ const asyncHandler = require('express-async-handler');
 const validateMongoDbId = require('../utils/validateMongodbId');
 const { generateRefreshToken } = require('../config/refreshToken');
 const jwt = require('jsonwebtoken')
+const sendEmail = require('./emailCtrl')
+const crypto = require('crypto')
 
 const createUser = asyncHandler(async (req, res) => {
     const email = req.body.email;
@@ -89,7 +91,7 @@ const logout = asyncHandler(async (req, res) => {
         });
         return res.sendStatus(204)
     }
-    await User.findOneAndUpdate({refreshToken}, {
+    await User.findOneAndUpdate({ refreshToken }, {
         refreshToken: "",
     })
 
@@ -204,5 +206,59 @@ const unBlockUser = asyncHandler(async (req, res) => {
     });
 })
 
+const updatePassword = asyncHandler(async (req, res) => {
+    const { _id } = req.user;
+    const { password } = req.body;
+    validateMongoDbId(_id);
+    const user = await User.findById(_id);
+    if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+    }
 
-module.exports = { createUser, loginUserCtrl, getallUsers, getaUser, deleteaUser, updatedUser, blockUser, unBlockUser, handleRefreshToken, logout }
+    if (password) {
+        user.password = password;
+        const updatedPassword = await user.save();
+        res.json(updatedPassword)
+    } else {
+        res.json(user);
+    }
+})
+
+const forgotPassowrdToken = asyncHandler(async (req, res) => {
+    const { email } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) throw new Error("User not found with this email");
+    try {
+        const token = await user.createPasswordResetToken();
+        await user.save();
+        const resetURL = `Hi, Please follow this link to reset Your Password. This link is valid till 10 minutes from now. <a href='http://localhost:5000/api/user/reset-password/${token}'>Click Here</>`;
+        const data = {
+            to: email,
+            text: "Hey User",
+            subject: "Forgot Password Link",
+            htm: resetURL,
+        };
+        sendEmail(data);
+        res.json(token);
+    } catch (error) {
+        throw new Error(error);
+    }
+})
+
+const resetPassword = asyncHandler(async (req, res) => {
+    const { password } = req.body;
+    const { token } = req.params;
+    const hashedToken = crypto.createHash("sha256").update(token).digest("hex");
+    const user = await User.findOne({
+      passwordResetToken: hashedToken,
+      passwordResetExpires: { $gt: Date.now() },
+    });
+    if (!user) throw new Error(" Token Expired, Please try again later");
+    user.password = password;
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save();
+    res.json(user);
+  });
+
+module.exports = { createUser, loginUserCtrl, getallUsers, getaUser, deleteaUser, updatedUser, blockUser, unBlockUser, handleRefreshToken, logout, updatePassword, forgotPassowrdToken, resetPassword }
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const { createUser, loginUserCtrl, getallUsers, getaUser, deleteaUser, updateaUser, updatedUser, blockUser, unBlockUser, handleRefreshToken, logout } = require('../controller/userCtrl');
+const { createUser, loginUserCtrl, getallUsers, getaUser, deleteaUser, updatedUser, blockUser, unBlockUser, handleRefreshToken, logout,updatePassword, forgotPassowrdToken, resetPassword } = require('../controller/userCtrl');
 const { authMiddleware, isAdmin } = require('../middlewares/authMiddleware');
 const router = express.Router();
 
 
 router.post('/register',createUser)
+router.post('/forgot-password-token', forgotPassowrdToken)
+router.put('/reset-password/:token', resetPassword)
+router.put('/password',authMiddleware,updatePassword)
 router.post('/login',loginUserCtrl)
 router.get('/all-users', getallUsers)
 router.get('/refresh',handleRefreshToken)
