a47ba28f6ea74e6fe0bc8d93f5f2a4384c6e78e3
Nobisuke19 <subham019650@gmail.com>
2024-11-01 23:44:40 +0530

    product-category

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ const PORT = process.env.PORT || 4000;
 const authRouter = require('./routes/authRoutes');
 const productRouter = require('./routes/productRoutes')
 const blogRouter = require('./routes/blogRoute')
+const categoryRouter = require('./routes/prodCategoryRoute')
+
 const { notFound, errorHandler } = require('./middlewares/errorHandler');
 const cookieParser = require('cookie-parser')
 const morgan = require('morgan')
@@ -22,6 +24,7 @@ app.use(morgan('dev'))
 app.use('/api/user', authRouter)
 app.use('/api/product', productRouter)
 app.use('/api/blog',blogRouter)
+app.use('/api/category', categoryRouter)
 
 app.use(notFound);
 app.use(errorHandler);
